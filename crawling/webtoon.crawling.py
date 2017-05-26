1a01db3ec21140c47682db14cb18d6abb2a256b3
junho Kwon <oilvers0904@gmail.com>
2017-05-26 18:12:08 +0900

    add naver webtoon crawling contents

--- crawling/webtoon.crawling.py
+++ crawling/webtoon.crawling.py
@@ -1,41 +1,185 @@
+import os
+
 import requests
 from bs4 import BeautifulSoup
 
-url_naver_webtoon_episode_list = 'http://comic.naver.com/webtoon/list.nhn'
-id_1 = 651673
-id_2 = 119874
 
-params_webtoon_episode_list = {
+class NaverWebtoonCrawler:
 
-    'titleId' : id_1,
-    'page' : 1,
-}
+    #초기화메서드에 넣지 않는 것은 바뀌지 않을 값이다.
+    _url_detail_base = 'http://comic.naver.com/webtoon/detail.nhn?' \
+                        #네이버 웹툰 메인 페이지
+                       'titleId={webtoon_id}&' \
+                        #어떤 웹툰 전체 페이지
+                       'no={episode_num}&' \
+                        # no = 58은 58화의 넘버
+                       'weekday=wed'
+                        #수요일웹툰
 
-# 이 작업들을 한 번에 처리해주는 함수를 구현
+    def __init__(self, webtoon_id):
+        self.webtoon_id = webtoon_id
+        #webtoon_id : webtoon_id매개변수에 실행인자를 넣은것을 받아 self자기자신.webtoon_id에 넣어준다
+        # 즉 자기자신 초기화 메서드를 통해 webtoon_id가 어떤값이 올수 있도록 초기화해준다.
+        #초기화메서드에 넣는 매개변수들은 계속 바뀌는 값이기에
+        # self.매개변수 = 매개변수로, 어떤매개변수가 올때마다 self자신의 값이 바뀌도록
+        # self.인스턴스이름 = 매개변수로 설정해준다
 
-def get_html_from_url(url,params):
+    def crawl_page(self, page_num):
+        return '해당 페이지의 에피소드 리스트'
 
-    # 실제 요청한 url(url과 params를 조합해서 만들어질 요청url을 출력
-    # 위의 두 변수 (url,params)는 사용해서는 안됨
-    response = requests.get(url,params)
+    def crawl_episode(self, episode_num=None):
+        """
+        webtoon_id에 해당하는 웹툰의 episode_num화 내부의 이미지를 저장
+        :param episode_num: 
+        :return: 
+        """
+        dir_path = '{}/{}'.format(self.webtoon_id, episode_num)
+        # 폴더경로는 webtoon_id는 어떤웹툰의 타이틀넘버 , episode_num은 각 화수의 넘버
 
-    # 요청한 url을 출력
-    # print(response.url)
+        def make_episode_dir():
+            # 이미지를 저장하기 위한 폴더 생성
+            print(dir_path)
+            # exists로 이미 생성하려는 폴더가 있는지 검사
+            if not os.path.exists(dir_path):
+                # os는 지금 돌고있는 클래스의 서버환경 path:환경의 경로 exists는 즉 os.path.exists는 정형화된 패턴
+                os.makedirs(dir_path)
+                # os.makedirs도 정형화된 패턴
+                print('dir created')
+            print('dir exist')
 
-    # 응답의 상태코드를 출력
-    # print(response.status_code)
+            # try~except구문으로 이미 폴더가 존재할경우 예외처리
+            # exists로 이미 생성하려는 폴더가 있는지 검사
+            try:
+                os.makedirs(dir_path)
+            except FileExistsError as e:
+                print('dir exist, error:', e)
 
-    # 응답의 텍스트 인코딩을 출력
-    # print(response.encoding)
+            # exists로 이미 생성하려는 폴더가 있는지 검사
+            # exist_ok매개변수 추가
+            os.makedirs(dir_path, exist_ok=True)
 
-    return response.text
+        def  get_img_tag_list():
+            """
+            디테일페이지에서 img Tag(bs4)의 리스트를 반환
+            :return: 
+            """
+            # 디테일 페이지의 html을 가져와 img의 href를 출력
+            # print(url_detail)
+            # requests를 이용해서 url_detail에 get요청을 보냄
+            response = requests.get(url_detail)
+            # with open('test.html', 'wt') as f:
+            #     f.write(response.text)
+            # 응답(Response)에서 .text를 이용해 내용을 가져옴
+            # 가져온 응답내용을 이용해 BeautifulSoup인스턴스를 생성 (soup)
 
-class Episode:
+            soup = BeautifulSoup(response.text, 'lxml')
+            # soup인스턴스에서 select_one 메서드를 사용해 웹툰뷰어 태그를 리턴
 
-    #url,title,rating,date는 프로퍼티구현(전부 읽기전용 ,private으로 선언하지 않음
+            div_wt_viewer = soup.select_one('div.wt_viewer')
+            # 웹튠뷰어 태그에서 img태그들을 전부 찾아 리스트로 반환
+            img_list = div_wt_viewer.find_all('img')
+            return img_list
 
+        if not episode_num:
+            url_detail = self._url_detail_base.format(
+                # self._url_detail_base는 초기화메서드 밖에 있는 것으로 네이버웹툰메인은 바뀔필요가 없다. 그래서
+                # 초기화메서드 밖에 써주는것이다. 쓸때는 자신이 자신의 값을 가지므로 self.url_detail_base이다
+                self.webtoon_id,
+                #format은 문자열을 처리해주는 방식으로 base(인스턴스).format함수안에 self.webtoon_id라는 타이틀명
+                #에 해당하는 것을 넣어 실행한다. 즉 url_detail은 기본메인페이지에서 어떤웹툰의 메인페이지까지 불러오는것이다
+                '마지막 에피소드 넘버 가져오기'
+            )
+        else:
+            url_detail = self._url_detail_base.format(
+
+            #     _url_detail_base='http://comic.naver.com/webtoon/detail.nhn?' \
+            #         # 네이버 웹툰 메인 페이지
+            #                      'titleId={webtoon_id}&' \
+            #         # 어떤 웹툰 전체 페이지
+            #                      'no={episode_num}&' \
+            #         # no = 58은 58화의 넘버
+            #                      'weekday=wed'
+            # 에 중괄호 되어있는 {wetoon_id}하고 {episode_num}이있기에 webtoon_id = self.webtoon_id, episode_num
+
+                webtoon_id=self.webtoon_id,
+                episode_num=episode_num
+                #어떤 웹툰페이지는 바뀌지만 아직까지는 num은 바꾸지 않았다.
+            )
+
+        # 이미지를 다운받을 폴더 생성
+        make_episode_dir()
+
+        # 이미지 태그 목록 가져오기
+        img_list = get_img_tag_list()
+
+        # 리스트를 순회하며 각 img태그의 src속성을 출력 및 다운로드
+        for index, img in enumerate(img_list):
+            # 이미지 주소에 get요청
+            print(img['src'])
+            #tag['속성']이면 속성에 해당하는 내용을 가져와라
+
+
+            headers = {'Referer': url_detail}
+            #네이버에서 관련된 url로 들어오게 만드는것
+
+            response = requests.get(img['src'], headers=headers)
+            # requests를
+
+
+            # 요청 결과 (이미지파일)의 binary데이터를 파일에 쓴다
+            img_path = '{}/{:02}.jpg'.format(
+                dir_path,
+                index
+            )
+            with open(img_path, 'wb') as f:
+                #wb는 이전데이터타입
+                f.write(response.content)
+
+        # 해당 에피소드를 볼 수 있는 HTML파일을 생성
+        html_path = '{}/{:02}.html'.format(self.webtoon_id,episode_num)
+
+        # 위 경로로 파일을 쓰기모드로 받고 '<html>'을 기록 후 닫기
+
+
+        with open(html_path,'wt') as f:
+            f.write('<html>')
+        """
+        <html>
+            <img src="651673/1/00.jpg">
+            <img src="651673/1/01.jpg">
+            <img src="651673/1/02.jpg">
+            <img src="651673/1/03.jpg">
+        </html>
+        """
+        print('Crawling complete')
+
+    def crawl_all_episodes(self):
+        return ''
+
+
+class NaverWebtoon:
+    def __init__(self, webtoon_id):
+        self.webtoon_id = webtoon_id
+        self.episode_list = []
+
+    def get_info(self):
+        return '정보 리턴'
+
+    def view_last_episode(self):
+        return ''
+
+    def view_episode_list(self, num=1):
+        return ''
+
+    def save_webtoon(self, create_html=False):
+        if create_html:
+            return '다운받은 웹툰을 볼 수 있는 html까지 생성해서 저장'
+        return '특정 경로에 웹툰 전체를 다운받아서 저장'
+
+
+class NaverWebtoonEpisode:
+    # url_thumbnail, title, rating, date는 property로 구현 (전부 읽기전용, private으로 선언하지 말 것)
     def __init__(self, url_thumbnail, title, rating, date):
-
         self._url_thumbnail = url_thumbnail
         self._title = title
         self._rating = rating
@@ -56,71 +200,3 @@ class Episode:
     @property
     def date(self):
         return self._date
-
-# 1. 전체 에피소드 리스트를 가져오기
-# 2. 에피소드 번호를 입력하면(no) 내부의 웹툰 이미지 주소를 가져오기 (여러장의 이미지로 분할되어있음)
-# 3. 썸네일이미지 또는 내부 웹툰 이미지를 저장
-# 4. 모든것을 통합해서 실행하면 웹툰을 각 화별 폴더를 생성해서 저장하기
-# 5. 저장한 파일을 접근할 수 있는 HTML생성
-
-html = get_html_from_url(url_naver_webtoon_episode_list, params_webtoon_episode_list)
-html = get_html_from_url(url_naver_webtoon_episode_list, params_webtoon_episode_list)
-
-soup = BeautifulSoup(html, 'lxml')
-
-div_comicinfo = soup.find('div', class_='comicinfo')
-
-div_comicinfo_detail = div_comicinfo.find('div', class_='detail')
-
-div_comicinfo_detail_h2 = div_comicinfo_detail.find('h2')
-
-div_comicinfo_detail_h2_wrt_nm = div_comicinfo_detail_h2.find('span', class_='wrt_nm')
-
-
-
-author_name = soup.select_one('div.comicinfo > div.detail > h2 > span.wrt_nm').text.strip()
-title = soup.select_one('div.comicinfo > div.detail > h2').contents[0].strip()
-#author_name = soup.select_one('div.comicinfo > div.detail > h2 > span > span.wrt_nm').text.strip()
-print(title, author_name)
-
-episode_list = []
-#에피소드 리스트를 가져온다
-# 각 에피소드는 class Episode형 인스턴스가 되도록 함
-# 각 에피소드의 썸네일 이미지 url, 제목, 별점 등록일을
-# url_thumbnail, title, rating, date에 등록
-
-table = soup.find('table', class_='viewList')
-tr_list = table.find_all('tr')
-for tr in tr_list:
-    if not tr.find('td',class_='title'):
-        continue
-    title = tr.find('td', class_='title').find('a').test
-    link = tr.find('td' ,class_='title').find('a')['href']
-    rating = tr.find_all('td')[2].find('strong').text
-
-    # td_list = tr.find_all('td')
-    # print(td_list)
-    # td_rating = td_list[2]
-    # print(td_rating)
-    # strong = td_rating.find('strong')
-    # print(strong)
-    # rating = strong.text.strip()
-
-    date = tr.find('td', class_='num').text
-
-
-    print(title)
-    print(link)
-    print(rating)
-    print(date)
-
-    episode = Episode(
-        url_thumbnail='',
-        title=title,
-        rating=rating,
-        date=date
-    )
-    episode_list.append(episode)
-
-for episode in episode_list:
-    print(episode)
